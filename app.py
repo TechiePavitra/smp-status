6f1759f280e6ee13bc7e8801855839dfad6995d8
TechiePavitra <techiepavitra@gmail.com>
2026-09-05 17:48:20 +0530

    Some Backend stuff changes

--- app.py
+++ app.py
@@ -38,5 +38,10 @@ def api_status():
     return jsonify(get_server_status())
 
 
+# Backend stuff
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
